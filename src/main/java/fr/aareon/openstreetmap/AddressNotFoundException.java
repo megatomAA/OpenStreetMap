5462999048adb68964da5c686762937feed6a767
Thomas Lassauniere <thomas.lassauniere@aareon.fr>
2018-09-10 10:54:32 +0200

    Added Exception class AddressNotFoundException Added image for marker

---
+++ src/main/java/fr/aareon/openstreetmap/AddressNotFoundException.java
@@ -0,0 +1,30 @@
+package fr.aareon.openstreetmap;
+
+public class AddressNotFoundException extends Exception {
+
+    public AddressNotFoundException() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public AddressNotFoundException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AddressNotFoundException(Throwable cause) {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AddressNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+
+    public AddressNotFoundException(String message, Throwable cause, boolean enableSuppression,
+                    boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        // TODO Auto-generated constructor stub
+    }
+
+}
